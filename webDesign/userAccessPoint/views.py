fd809c4c17ddd06eccb40260f41b8a32c7c51923
Steve Mark <105312132+WainainaMark@users.noreply.github.com>
2025-02-15 14:25:04 +0000

    Sigin Form and Design part 1

--- webDesign/userAccessPoint/views.py
+++ webDesign/userAccessPoint/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
 # Create your views here.
 def signIn(request):
-    return render(request, 'uap/signIn.html')
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after successful registration
+            return redirect('home')  # Redirect to a home page after sign-up
+    else:
+        form = SignUpForm()
+    return render(request, 'uap/signIn.html', {'form': form})
